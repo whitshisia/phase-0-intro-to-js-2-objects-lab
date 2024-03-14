c10e2e845c7a7a1e90167eb7199060c8e3d55722
whitney shisia <shisiawhitney215@gmail.com>
2024-03-14 17:40:37 +0300

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution in this file!
+const employee = {
+    name: 'John',
+    age: 30,
+    role: 'Software Engineer'
+  };
+  
+  function updateEmployeeWithKeyAndValue(employee,key,value) {
+    const updatedEmployee = {...employee};
+        updatedEmployee[key] = value;
+    return updatedEmployee;
+  }
+  function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee[key] = value;
+    return employee;
+  }
+//   module.exports = destructivelyUpdateEmployeeWithKeyAndValue; 
+function deleteFromEmployeeByKey(employee, key) {
+    const employeeClone = { ...employee };
+    delete employeeClone[key];
+    return employeeClone;
+}
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    if (employee && employee[key]) {
+        delete employee[key];
+    }
+    return employee;
+}
